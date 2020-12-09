7062b627e075e5ec7c2b81cfeb85e5e7102719e3
Prabhat Agarwal <prabhat.agarwal0605@gmail.com>
2020-12-09 21:08:59 +0530

    fixed code issues

--- src/main/java/com/rest/micronaut/repository/FileRepository.java
+++ src/main/java/com/rest/micronaut/repository/FileRepository.java
@@ -1,17 +1,11 @@
 package com.rest.micronaut.repository;
 
-import com.rest.micronaut.Entity.FileEntity;
 
-import io.micronaut.context.annotation.Executable;
+import com.rest.micronaut.entity.FileEntity;
+
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.repository.CrudRepository;
 
 @Repository
 public interface FileRepository extends CrudRepository<FileEntity, Long> {
-	
-	@Executable
-	public FileEntity findByFileId(Long id);
-	
-	@Executable
-	public void deleteByFileId(Long id);
 }
